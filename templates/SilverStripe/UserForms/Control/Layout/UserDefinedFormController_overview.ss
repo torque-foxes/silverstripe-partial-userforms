b67ebc57a8e808f865ef5020598d88b30b913433
Sheila Bañez <sheila@silverstripe.com>
2019-09-10 22:48:14 +1200

    MMS-117: Display lockout message

--- templates/SilverStripe/UserForms/Control/Layout/UserDefinedFormController_overview.ss
+++ templates/SilverStripe/UserForms/Control/Layout/UserDefinedFormController_overview.ss
@@ -3,4 +3,8 @@
 
     <p>$FormOverview</p>
     $Form
+
+    <% if $FormLocked %>
+        <p>This form is currently being used by someone else. Please try again in 30 minutes.</p>
+    <% end_if %>
 </div>
--- tests/unit/PartialUserFormControllerTest.php
+++ tests/unit/PartialUserFormControllerTest.php
@@ -2,9 +2,11 @@
 
 namespace Firesphere\PartialUserforms\Tests;
 
+use Firesphere\PartialUserforms\Controllers\PartialSubmissionController;
 use Firesphere\PartialUserforms\Models\PartialFormSubmission;
 use SilverStripe\Assets\File;
 use SilverStripe\Dev\FunctionalTest;
+use SilverStripe\ORM\FieldType\DBDatetime;
 use SilverStripe\UserForms\Model\UserDefinedForm;
 
 /**
@@ -96,11 +98,78 @@ class PartialUserFormControllerTest extends FunctionalTest
         $this->assertContains($formOpeningTag, $result->getBody());
     }
 
-    /**
-     * TODO: Add test when MMS-115 is complete
-     */
     public function testIsLockedOut()
     {
+        $partial = $this->objFromFixture(PartialFormSubmission::class, 'submission1');
+        $this->session()->set(PartialSubmissionController::SESSION_KEY, $partial->ID);
+
+        // No session recorded = not locked
+        $response = $this->get($partial->parent()->Link('overview'));
+        $this->assertNull($partial->LockedOutUntil);
+        $this->assertNull($partial->PHPSessionID);
+        $this->assertNotContains('This form is currently being used by someone else', $response->getBody());
+
+        // Session in the past + same user = Not Locked
+        DBDatetime::set_mock_now('2019-02-15 11:00:00');
+        session_id('white');
+        $partial->LockedOutUntil = '2019-02-15 09:00:00';
+        $partial->PHPSessionID = 'white';
+        $partial->write();
+
+        $response = $this->get($partial->parent()->Link('overview'));
+        $this->assertNotContains('This form is currently being used by someone else', $response->getBody());
+        // -- Check for updated record
+        $updatedPartial = PartialFormSubmission::get()->byID($partial->ID);
+        $this->assertEquals('2019-02-15 11:30:00', $updatedPartial->LockedOutUntil);
+        $this->assertEquals('white', $updatedPartial->PHPSessionID);
+
+        // Session in the past + different user = Not Locked
+        DBDatetime::set_mock_now('2019-02-15 11:00:00');
+        session_id('red');
+        $partial = PartialFormSubmission::get()->byID($partial->ID); // update the latest partial
+        $partial->LockedOutUntil = '2019-02-15 09:00:00';
+        $partial->PHPSessionID = 'white';
+        $partial->write();
+
+        $response = $this->get($partial->parent()->Link('overview'));
+        $this->assertNotContains('This form is currently being used by someone else', $response->getBody());
+        // -- Check for updated record
+        $updatedPartial = PartialFormSubmission::get()->byID($partial->ID);
+        $this->assertEquals('2019-02-15 11:30:00', $updatedPartial->LockedOutUntil);
+        $this->assertEquals('red', $updatedPartial->PHPSessionID);
+
+        // Session within 30mins + same user = Not Locked
+        DBDatetime::set_mock_now('2019-02-15 12:00:00');
+        session_id('blue');
+        $partial = PartialFormSubmission::get()->byID($partial->ID); // update the latest partial
+        $partial->LockedOutUntil = '2019-02-15 12:15:00';
+        $partial->PHPSessionID = 'blue';
+        $partial->write();
+
+        $response = $this->get($partial->parent()->Link('overview'));
+        $this->assertNotContains('This form is currently being used by someone else', $response->getBody());
+        // -- Check for updated record
+        $updatedPartial = PartialFormSubmission::get()->byID($partial->ID);
+        $this->assertEquals('2019-02-15 12:30:00', $updatedPartial->LockedOutUntil);
+        $this->assertEquals('blue', $updatedPartial->PHPSessionID);
+
+        // Session within 30mins + different user = Locked
+        DBDatetime::set_mock_now('2019-02-15 12:00:00');
+        session_id('black');
+        $partial = PartialFormSubmission::get()->byID($partial->ID); // update the latest partial
+        $partial->LockedOutUntil = '2019-02-15 12:15:00';
+        $partial->PHPSessionID = 'white';
+        $partial->write();
+
+        $response = $this->get($partial->parent()->Link('overview'));
+        $this->assertContains('This form is currently being used by someone else', $response->getBody());
+        // -- Check for unchanged record
+        $updatedPartial = PartialFormSubmission::get()->byID($partial->ID);
+        $this->assertEquals('2019-02-15 12:15:00', $updatedPartial->LockedOutUntil);
+        $this->assertEquals('white', $updatedPartial->PHPSessionID);
+
+        // Set the session back to empty string to prevent destroying uninitialized session
+        session_id('');
     }
 
     public function setUp()
